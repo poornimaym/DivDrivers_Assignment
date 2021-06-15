be9482d8a33fd303d3b246104ae6630754c86044
poornimaym <85963707+poornimaym@users.noreply.github.com>
2021-06-16 01:56:53 +0530

    Day1_Assignments

---
+++ assgn_2.c
@@ -0,0 +1,7 @@
+/*#include <stdio.h>
+
+main()
+{
+}*/
+
+// a .c file expects atleast a main() function call so it gives error
---
+++ nest_comment.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	/*comment1 /*comment2 */*/
+	return 0;
+}
+
+/* once the comment(/*) is started the compiler look for nearest ending '*/' this leads to uncommenting of somw of the comments which might leads to unnecessary trouble/Error so Line by line Commenting is good. */
